b289301f0e548f91138dad4602afadab6ac66e69
hattori_akio@hotmail.com <hattori_akio@hotmail.com>
2022-08-11 20:08:52 -0300

    Subindo aula 17 de Python

---
+++ Aulas/Aula 17/index..py
@@ -0,0 +1,109 @@
+# Jogo da Velha
+
+'''       0   1   2
+    0   [ 1 , 2 , 3 ]
+    1   [ 1 , 2 , 3 ]
+    2   [ 1 , 2 , 3 ]
+
+    Condição de vitoria
+    linha
+    X X X
+
+    Coluna
+    X
+    X
+    X
+
+    Diagonal
+    X
+        X
+            X
+            X
+        X
+    X   
+'''
+
+from calendar import TUESDAY
+
+
+tabuleiro = [
+    [ 1 , 2 , 3 ],
+    [ 4 , 5 , 6 ],
+    [ 7 , 8 , 9 ]
+]
+
+board = []
+cont = 1
+for i in range(3):
+    b = []
+    for j in range(3):
+        b.append(cont)
+        cont += 1
+    board.append(b[:])
+
+# print(tabuleiro)
+# print(board)
+
+def listar_tabuleiro():
+    for i in tabuleiro:
+        print(i)
+
+def pergunta_jogada():
+    while(True):
+        jogada = int(input('Onde deseja jogar? '))
+        if verifica_vazio(jogada):
+            return jogada
+
+def verifica_vazio(valor):
+    for i in tabuleiro:
+        for j in i:
+            if j == valor:
+                return True
+    return False
+
+def verificaVitoria():
+    #linha
+    for i in range(len(tabuleiro)):
+        if tabuleiro[i][0] == tabuleiro[i][1] and tabuleiro[i][0] == tabuleiro[i][1]:
+            return True
+        print('verifica colunas')
+        print(tabuleiro[0][i], tabuleiro[1][i], tabuleiro[0][i])
+        if (tabuleiro[0][i] == tabuleiro[1][i] and tabuleiro[0][i] == tabuleiro[2][i]):
+            return True
+    if(tabuleiro[0][0] == tabuleiro[1][1] and tabuleiro[0][0] == tabuleiro[2][2]):
+        return True
+    if(tabuleiro[0][2] == tabuleiro[1][1] and tabuleiro[0][2] == tabuleiro[2][0]):
+        return True
+
+def alterarCampo(valor, letra):
+    for i in range(len(tabuleiro)):
+        if tabuleiro[i][0] == valor:
+            tabuleiro[i][0] = letra
+        elif tabuleiro[i][1] == valor:
+            tabuleiro[i][1] = letra
+        elif tabuleiro[i][2] == valor:
+            tabuleiro[i][2] = letra
+
+jogadas = 0
+while(True):
+    listar_tabuleiro()
+    if(jogadas % 2 == 0):
+        print("jogador 1")
+        alterarCampo(pergunta_jogada()), "X"
+        if verificaVitoria():
+            print('Jogador 1 ganhou a partida! ')
+            break
+    else:
+        print('jogador 2')
+        alterarCampo(pergunta_jogada()), "O"
+        if verificaVitoria():
+            print('Jogador 2 ganhou a partida! ')
+            break
+
+    jogadas += 1
+    if jogadas == 9:
+        print('Deu velha! ')
+        break
+
+
+    
