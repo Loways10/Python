3bc72a4e4b44323791734e33018796fcefb4866f
hattori_akio@hotmail.com <hattori_akio@hotmail.com>
2022-08-23 18:03:36 -0300

    atualizando a aula 18 de python

--- Aulas/Aula 18/produto.py
+++ Aulas/Aula 18/produto.py
@@ -1,8 +1,51 @@
+from random import randint
+
 class Produto:
-    def __init__(self, codigo, nome, preco, qntEstoque, tipo):
-        self.codigo = codigo
-        self.nome = nome
-        self.preco = preco
-        self.qntEstoque = qntEstoque
-        self.tipo = tipo
+    lista_tipos = ["Alimmentação", "Limmpeza", "Pets"]
+
+    def __init__(self, n, p, qnt, t):
+        self.codigo = self.gerarCodigoProduto()
+        self.nome = n
+        self.preco = p
+        self.qntEstoque = qnt
+        self.tipo = t
+
+    def listarProduto(self):
+        print(f'Codigo: {self.codigo}')
+        print(f'Nome: {self.nome}')
+        print(f'R$: {self.preco}')
+        print(f'Quantidade Estoque: {self.qntEstoque}')
+        print(f'Tipo: {self.retornarTipo(self.tipo)}')
+        print('--' * 15)
+
+    def valorEstoque(self):
+        return print(f'Total estoque R$ {self.preco * self.qntEstoque}')
+
+    def venderProduto(self, qnt):
+        if qnt > self.qntEstoque:
+            return print('Quantidade de estoque insuficiente')
+        self.qntEstoque -= qnt
+        self.listarProduto()
+        print(f'Venda realizada com sucesso! Restam {self.qntEstoque} produtos em estoque')
+        self.verificarComprarMais(self.qntEstoque)
+
+    @classmethod
+    def listarTipos(cls):
+        print(cls.lista_tipos)
+
+    @classmethod
+    def retornarTipo(cls, tipo):
+        return cls.lista_tipos[tipo - 1]
+
+    @staticmethod
+    def verificarComprarMais(qnt):
+        if qnt < 5:
+            print('Compre mais produtos')
+        else:
+            print(('Ainda há produtos no estoque'))
+
+    @staticmethod
+    def gerarCodigoProduto():
+        return randint(10000, 99999)
+
         
