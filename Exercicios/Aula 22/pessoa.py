16e22ac3888bc48b46bb517684fd1c16373e306c
hattori_akio@hotmail.com <hattori_akio@hotmail.com>
2022-08-30 17:34:00 -0300

    Subindo exercicio aula 22 de python completo

--- Exercicios/Aula 22/pessoa.py
+++ Exercicios/Aula 22/pessoa.py
@@ -17,7 +17,28 @@ class Professor(Pessoa):
 
 
 class Aluno(Pessoa):
-    pass
+    def __init__(self, nome, ano_nascimento, ano_escolar):
+        super().__init__(nome)
+        self.ano_nascimento = ano_nascimento
+        self.ano_escolar = ano_escolar
+
+    def listar_aluno(self):
+        print(f'{self.nome}')
+        print(f'{self.ano_nascimento}')
+        print(f'{self.ano_escolar}')
+
+    def alterar_ano_escolar(self, valor):
+        self.ano_escolar = valor
+
+    def idade_aluno(self):
+        print(f'{self.ano_escolar - self.ano_nascimento}')
 
 p = Professor('Akio', '13781388727', 1)
 p.listar_professor()
+print('---' * 15)
+a = Aluno('Kenji', 1994, 2019)
+a.listar_aluno()
+print('---' * 15)
+a.alterar_ano_escolar(2022)
+a.listar_aluno()
+a.idade_aluno()
